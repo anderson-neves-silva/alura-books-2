cae93f9607511889ffac19e1b4776b06ebbb29db
Anderson Neves <andersonneves_@hotmail.com>
2022-11-11 18:43:21 -0300

    implementando o mostrar apenas os livros disponíveis

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,6 +4,6 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));  // signifi
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id);  // qual o botão está sendo clicado.
     const categoria = elementoBtn.value;
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria);
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);  // operador ternário para mostrar os livros disponível, se a categoria for igual a disponível fazer um filtro dos livros que tenha a quantidade for maior que “ 0 “, se não “ : “ fazer o filtro normal da categoria.
     exibirOsLivrosNaTela(livrosFiltrados);  // como já temos a função que exibe os livros na tela, eu apenas chamo ela aqui, note que os livros estão se repetindo na tela após clicar novamente no mesmo botão, então o que preciso é apenas ir na função exibirLivrosNaTela e fazer uma chamada de innerHTML passando vazio no início dela.
 }
